a5d1e7e682ca5b1cae00cf9fb75da834eb33a9f1
Ryan Ferguson <raferguson91@gmail.com>
2024-06-15 15:11:11 -0500

    committing for push to rep

--- errors-and-debugging/exercises/Debugging1stSyntaxError.js
+++ errors-and-debugging/exercises/Debugging1stSyntaxError.js
@@ -4,8 +4,9 @@
 let launchReady = false;
 let fuelLevel = 17000;
 
-if (fuelLevel >= 20000 {
-   console.log('Fuel level cleared.');
+if (fuelLevel >= 20000) {
+
+   console.log('Fuel level cleared.'); 
    launchReady = true;
 } else {
    console.log('WARNING: Insufficient fuel!');
