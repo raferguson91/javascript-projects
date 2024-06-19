cb957997662185eb45efd560427c07f08d41e888
Ryan Ferguson <raferguson91@gmail.com>
2024-06-19 18:30:36 -0500

    part three exercise

--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -4,14 +4,25 @@ let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 
+console.log(language.slice(0,1)+language.slice(4,5));
+
 //2. Without using slice(), use method chaining to accomplish the same thing.
+
+console.log(language.charAt(0)+language.charAt(4));
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
 
+let firstInitial = language[0];
+let fifthInitial = language[4];
+console.log(`The abbreviation for ${language} is ${firstInitial}${fifthInitial}.`);
+
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
 
+console.log(language.slice(0,1) + language.indexOf("p") + language.charAt(3));
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+console.log(notTitleCase.replace("t","T").replace("c", "C"));
+
