2b7ee7ad257cca6e808c13ff8e222486ef004596
Ryan Ferguson <raferguson91@gmail.com>
2024-06-19 18:05:05 -0500

    part two exercise

--- stringing-characters-together/exercises/part-two.js
+++ stringing-characters-together/exercises/part-two.js
@@ -38,6 +38,11 @@ if (dnaTwo.indexOf('CAT') > -1) {
 
 //3) Use .slice() to print out the fifth gene (set of 3 characters) from the DNA strand.
 
+console.log(dna.slice(16,19));
+
 //4) Use a template literal to print, "The DNA strand is ___ characters long."
 
+console.log(`The DNA Strand is ${dnaTwo.length} characters long.`);
+
 //5) Just for fun, apply methods to ``dna`` and use another template literal to print, 'taco cat'.
+console.log(`${dna.slice(4,7).toLowerCase()}o ${dna.slice(dna.indexOf('CAT'),dna.indexOf('CAT')+3).toLowerCase()}`);
