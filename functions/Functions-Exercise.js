d7e58bde03c6d14f10b27906286a4f56bbb65cab
Ryan Ferguson <raferguson91@gmail.com>
2024-06-30 13:17:23 -0500

    functions exercise

--- functions/Functions-Exercise.js
+++ functions/Functions-Exercise.js
@@ -1,25 +1,52 @@
-function makeLine(size) {
-    let line = '';
-    for (let i = 0; i < size; i++) {
-      line += '#';
-    }
-    return line;
-  }
- 
-  function makeSquare(size) {
-    let square = '';
+function makeLine (size) {
+    let str = " ";
     for (let i =0; i < size; i++) {
-        square += (makeLine(size) + '\n');
+       str += "#";
+    }
+    return str;
+}
+
+
+function makeSquare (size) {
+    let row = "";
+    for (let i = 0; i < size; i++) {
+        row += (makeLine(size) + '\n');
 
     }
-return square
+    return row;
+}
+console.log(makeSquare(5));
 
-  }
-
-  function makeRectangle(width, height) {
+function makeRectangle(width, height) {
     let rectangle = '';
     for (let i = 0; i < height; i++) {
       rectangle += (makeLine(width) + '\n');
     }
-    return rectangle.slice(0, -1);
+    return rectangle;
   }
+  console.log(makeRectangle(4,6));
+
+function makeDownwardStairs(height) {
+    let level = "";
+    for (let i =0; i < height; i++) {
+        level += (makeLine(i+1) + '\n');
+    }
+    return level
+}
+console.log(makeDownwardStairs(8));
+
+function makeSpaceLine (numSpaces, numChars) {
+
+}
+
+
+function makeIsoscelesTriangle(height) {
+    let triangle = '';
+    for (let i = 0; i < height; i++) {
+      triangle += (makeSpaceLine(height - i - 1, 2*i + 1) + '\n');
+    }
+    return triangle.slice(0, -1);
+  }
+
+  
+
